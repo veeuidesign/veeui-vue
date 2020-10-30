8dad435010ee4832ccc6454cbdd00375dce4e193
wildwood <lihq151@chinaunicom.cn>
2020-10-30 20:10:35 +0800

    feat: 新增Icon组件

---
+++ src/packages/Icon/icon.vue
@@ -0,0 +1,31 @@
+<template>
+    <svg class="vee-icon" aria-hidden="true" @click="$emit('click', $event)">
+        <use :xlink:href="`#icon${icon}`"></use>
+    </svg>
+</template>
+
+<script lang="ts">
+import '@/styles/icon';
+import { computed, defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'vee-icon',
+  props: {
+    icon: {
+        type: String,
+        required: true
+    }
+  },
+  components: {
+    
+  },
+});
+</script>
+
+<style lang="scss">
+.vee-icon {
+    width: 25px;
+    height: 25px;
+    vertical-align: middle;
+}
+</style>
